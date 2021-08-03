f4a3741782d1d292950cb55ff3a17ad636e0dda1
AustEcon <austecon0922@gmail.com>
2021-08-03 13:39:35 +1200

    (Linux) Install all python-based components in a portable manner & test functionality
    - Use os.pathsep in sitecustomize.py for cross-platform
    - Todo - add a --portable flag which searches ascending directories until it hits the "ElectrumSV-SDK" dir.

--- contrib/sitecustomize.py
+++ contrib/sitecustomize.py
@@ -2,9 +2,8 @@ import sys
 import os
 from pathlib import Path
 
-for env_var in os.environ.get('PYTHONPATH', "").split(";"):
+for env_var in os.environ.get('PYTHONPATH', "").split(os.pathsep):
     if env_var.strip() != "":
         sys.path.insert(0, str(Path(env_var)))
 
 sys.path.insert(0, '')
-
