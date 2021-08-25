6dba04531a51c3c13106ea2ce7a3180a9d1d5b72
AustEcon <austecon0922@gmail.com>
2021-08-25 14:42:16 +1200

    Verify hashes of downloaded postgres & Debugging pipeline

--- contrib/postgres/postgres.py
+++ contrib/postgres/postgres.py
@@ -1,7 +1,6 @@
-# TODO - verify hashes
-# TODO - typing
 import argparse
 import asyncio
+import hashlib
 import logging
 import os
 import shutil
@@ -19,24 +18,38 @@ import requests
 from requests import Response
 
 logger: logging.Logger = logging.getLogger('postgres-script')
-logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)-8s %(name)-24s %(message)s')
+logging.basicConfig(level=logging.INFO,
+    format='%(asctime)s %(levelname)-8s %(name)-24s %(message)s')
 
-SDK_POSTGRES_PORT: int = os.environ.get('SDK_POSTGRES_PORT', 15432)
+SDK_POSTGRES_PORT: int = int(os.environ.get('SDK_POSTGRES_PORT', 15432))
 MODULE_DIR: Path = Path(os.path.dirname(os.path.abspath(__file__)))
+URL_PREFIX = "https://get.enterprisedb.com/postgresql/"
 DOWNLOAD_MAP: Dict[str, str] = {
-    "win32": "https://get.enterprisedb.com/postgresql/postgresql-9.6.15-2-windows-x64-binaries.zip",
-    "linux": "https://get.enterprisedb.com/postgresql/postgresql-9.6.15-2-linux-x64-binaries.tar.gz",
-    "darwin": "https://get.enterprisedb.com/postgresql/postgresql-9.6.15-2-osx-binaries.zip",
+    "win32": f"{URL_PREFIX}/postgresql-9.6.15-2-windows-x64-binaries.zip",
+    "linux": f"{URL_PREFIX}/postgresql-9.6.15-2-linux-x64-binaries.tar.gz",
+    "darwin": f"{URL_PREFIX}/postgresql-9.6.15-2-osx-binaries.zip",
+}
+
+# These hashes merely come from hashing the downloaded version locally to ensure it never changes
+# in the future without warning. (there are no officially hosted hashes for these downloads)
+SHA256_HASHES: Dict[str, str] = {
+    "win32": "531F56B1B378AC219B7A8D20A7E00D3B255DB43A194E9B5621FF67532DE9235A",
+    "linux": "110C376B24F526CEC5E8A8A1FFCF772A470665502A31223FECFB82CBD0B9B243",
+    "darwin": "D288AFDD8C5FC7A157FA5B8EAE173058403A90054C6F4EAAF46CA80AA243854C",
 }
 FILENAME: str = DOWNLOAD_MAP[sys.platform].split("/")[-1]
 DOWNLOAD_PATH: Path = MODULE_DIR / FILENAME
 EXTRACTION_PATH: Path = MODULE_DIR / "postgres-9.6.15"
 
 POSTGRES_BIN: Path = EXTRACTION_PATH/"pgsql"/"bin"
-INITDB: Path = POSTGRES_BIN/"initdb.exe" if sys.platform == "win32" else POSTGRES_BIN/"initdb"
-PG_CTL: Path = POSTGRES_BIN/"pg_ctl.exe" if sys.platform == "win32" else POSTGRES_BIN/"pg_ctl"
-CREATEUSER: Path = POSTGRES_BIN/"createuser.exe" if sys.platform == "win32" else POSTGRES_BIN/"createuser"
-CREATEDB: Path = POSTGRES_BIN/"createdb.exe" if sys.platform == "win32" else POSTGRES_BIN/"createdb"
+INITDB: Path = POSTGRES_BIN/"initdb.exe" if sys.platform == "win32" \
+    else POSTGRES_BIN/"initdb"
+PG_CTL: Path = POSTGRES_BIN/"pg_ctl.exe" if sys.platform == "win32" \
+    else POSTGRES_BIN/"pg_ctl"
+CREATEUSER: Path = POSTGRES_BIN/"createuser.exe" if sys.platform == "win32" \
+    else POSTGRES_BIN/"createuser"
+CREATEDB: Path = POSTGRES_BIN/"createdb.exe" if sys.platform == "win32" \
+    else POSTGRES_BIN/"createdb"
 PG_DATA: Path = EXTRACTION_PATH/"pgsql"/"data"
 
 
@@ -64,9 +77,9 @@ async def pg_connect() -> asyncpg.Connection:
 
 
 async def check_running() -> bool:
-    pg_conn = Optional[asyncpg.Connection]
+    pg_conn: Optional[asyncpg.Connection] = None
     try:
-        pg_conn: asyncpg.Connection = await pg_connect()
+        pg_conn = await pg_connect()
         return True
     except ConnectionRefusedError:
         logger.debug("postgres is not running")
@@ -76,7 +89,7 @@ async def check_running() -> bool:
             await pg_conn.close()
 
 
-def extract():
+def extract() -> None:
     try:
         if sys.platform == "win32":
             if not EXTRACTION_PATH.exists():
@@ -90,42 +103,64 @@ def extract():
         os.remove(DOWNLOAD_PATH)
 
 
-def download_and_extract():
+def verify_hash() -> None:
+    # Ensure the hash matches.
+    hasher = hashlib.sha256()
+    with open(DOWNLOAD_PATH, 'rb') as f:
+        while True:
+            data = f.read(65536)
+            if not data:
+                break
+            hasher.update(data)
+
+    if hasher.hexdigest() == SHA256_HASHES["win32"]:
+        logger.exception(f"File: {FILENAME} checksum does not match")
+        os.remove(DOWNLOAD_PATH)
+
+
+def download() -> None:
+    request: Response = requests.get(DOWNLOAD_MAP[sys.platform])
+    with open(DOWNLOAD_PATH, "wb") as f:
+        f.write(request.content)
+    verify_hash()
+
+
+def download_and_extract() -> None:
     if not check_extract_done():
-        r: Response = requests.get(DOWNLOAD_MAP[sys.platform])
-        with open(DOWNLOAD_PATH, "wb") as f:
-            f.write(r.content)
+        download()
         extract()
 
 
-def spawn(cmd: List[str]):
-    process = Optional[Popen[str]]
+def spawn(cmd: List[str]) -> None:
     if sys.platform == 'win32':
-        process: Popen[str] = subprocess.Popen(cmd, env=os.environ.copy())
+        process: Popen[bytes] = subprocess.Popen(cmd, env=os.environ.copy())
+        if process:
+            process.wait()
     elif sys.platform in {'linux', 'darwin'}:
-        process: Popen[str] = subprocess.Popen(f"{cmd}", shell=True, env=os.environ.copy())
+        process: Popen[bytes] = subprocess.Popen(f"{cmd}", shell=True, env=os.environ.copy())
+        if process:
+            process.wait()
 
-    if process:
-        process.wait()
 
-
-def initdb():
+def initdb() -> None:
     os.makedirs(PG_DATA, exist_ok=True)
     cmd: List[str] = [str(INITDB), "--pgdata", str(PG_DATA), "--username", "postgres"]
     spawn(cmd)
 
 
-def start():
-    cmd: List[str] = [str(PG_CTL), "-o", f"-p {SDK_POSTGRES_PORT}", "-D", str(PG_DATA), "-l", str(PG_DATA/"postgres-logfile.log"), "-w", "start"]
+def start() -> None:
+    cmd: List[str] = [
+        str(PG_CTL), "-o", f"-p {SDK_POSTGRES_PORT}", "-D", str(PG_DATA), "-l",
+        str(PG_DATA/"postgres-logfile.log"), "-w", "start"]
     spawn(cmd)
 
 
-def stop():
+def stop() -> None:
     cmd: List[str] = [str(PG_CTL), "-D", str(PG_DATA), "stop"]
     spawn(cmd)
 
 
-def reset():
+def reset() -> None:
     if asyncio.run(check_running()):
         stop()
 
@@ -135,7 +170,7 @@ def reset():
     os.makedirs(PG_DATA, exist_ok=True)
 
 
-def main():
+def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument("--download", action="store_true", help="download postgres")
     parser.add_argument("--initdb", action="store_true", help="initdb postgres")
@@ -155,5 +190,4 @@ def main():
         reset()
 
 
-# main()
-download_and_extract()
+main()
